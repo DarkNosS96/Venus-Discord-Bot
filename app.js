b5e3965cfd21b4dea8a2f2d244331d144bbc9b7a
DarkNosS96 <philipemmil@gmail.com>
2019-02-07 22:55:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ client.on("message", async message => {
 			return message.channel.send("https://venusrust.eu/store");
 		}
 		if(cmd === `!wipe`){
-			return message.channel.send("The last wipe was on **Thu 31 Jan **. The next wipe will be on **Thu 7 Feb**. (Around 19:00 CET.)");
+			return message.channel.send("The last wipe was on **Thu 7 Feb **. The next wipe will be on **Thu 14 Feb**. (Around 19:00 CET.)");
 		}
 		if(cmd === `!steam`){
 			return message.channel.send("https://venusrust.eu/steam");
