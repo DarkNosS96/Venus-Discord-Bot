53fe6a6d6d5330b997b29d9759495f06c48fa09b
DarkNosS96 <philipemmil@gmail.com>
2019-03-02 00:53:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,87 @@ const enableRcon = process.env.enableRcon || config.enableRcon;
 const prefix = process.env.prefix || config.prefix;
 const roles = process.env.roles || config.roles;
 const { Client, MessageAttachment } = require('discord.js');
+const fs = require("fs");
+
+let channels;
+let data = null;
+let voiceLogChannel = "";
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+	// Check if voiceLogChannel has been set
+	if (data.voiceLogChannel === "")
+		return;
+
+	let username = oldMember.displayName;
+	let oldVCID = oldMember.voiceChannelID;
+	let newVCID = newMember.voiceChannelID;
+
+	let oldChannelName = (oldVCID != null && typeof oldVCID != undefined) ? channels.get(oldVCID).name : null;
+	let newChannelName = (newVCID != null && typeof newVCID != undefined) ? channels.get(newVCID).name : null;
+	
+	if (oldChannelName === null)
+		voiceLogChannel.sendMessage(`${username} connected to voice and joined ${newChannelName}`);
+	else if (newChannelName === null)
+		voiceLogChannel.sendMessage(`${username} disconnected`);
+	else
+		voiceLogChannel.sendMessage(`${username} moved to ${newChannelName}`);
+});
+
+client.on('channelCreate', (channel) => {
+	updateChannels();
+});
+
+client.on('channelDelete', (channel) => {
+	updateChannels();
+});
+
+let readdata = function () {
+	data = JSON.parse(fs.readFileSync("data.json"));
+	token = data.token;
+	prefix = data.prefix;
+}
+
+let writedata = function () {
+	fs.writeFile('data.json', JSON.stringify(data), 'utf8', () => {
+		console.log('Data written successfully!');
+	});
+}
+
+let updateChannels = function () {
+	channels = client.channels;
+}
+
+let setLogChannel = function (channel, msgChannel) {
+	voiceLogChannel = channels.get(channels.findKey('name', channel));
+
+	// Return if no channel has been set
+	if (typeof voiceLogChannel === 'undefined')
+		return;
+	else if (typeof voiceLogChannel === 'undefined' && msgChannel) {
+		msgChannel.sendMessage(`Couldn't find channel '${channel}'`);
+		return;
+	}
+	else if (voiceLogChannel.type === 'voice' && msgChannel) {
+		msgChannel.sendMessage(`Can only log to text channels`);
+		return;
+	}
+
+	data.voiceLogChannel = channel;
+
+	if (msgChannel)
+		msgChannel.sendMessage(`Channel for logging set to '${channel}'`);
+}
+
+readdata();
+
+
+
+
+
+
+
+
+
 
 var updateInterval = (1000 * 60) * 6;
 
