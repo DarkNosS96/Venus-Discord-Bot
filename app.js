8c2ce90b6ec24c7a0f7b3572b41f062a1d708480
DarkNosS96 <philipemmil@gmail.com>
2019-04-04 06:05:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,6 @@ const prefix = process.env.prefix || config.prefix;
 const roles = process.env.roles || config.roles;
 const { Client, MessageAttachment } = require('discord.js');
 
-
-const BaseCommand = require('../structures/BaseCommand');
-const fs = require('fs');
-const path = require('path');
-const svg2png = require('svg2png');
-const twemoji = require('twemoji');
-
-
 var updateInterval = (1000 * 60) * 6;
 const client = new Discord.Client();
 
@@ -103,73 +95,7 @@ if (enableRcon == 1)
   else if (debug) console.log("Rcon mode disabled")
 
   
-class EmojiCommand extends BaseCommand {
-    constructor() {
-        super({
-            name: 'emoji',
-            aliases: ['e'],
-            category: bu.CommandType.GENERAL,
-            usage: 'emoji <emoji> [size]',
-            info: 'Gives you a large version of an emoji. If size is specified and the emoji is not a custom emoji, the image will be that size.',
-            flags: [{
-                flag: 's',
-                word: 'svg',
-                desc: 'Get the emote as an svg instead of a png.'
-            }]
-        });
-    }
-
-    async execute(msg, words, text) {
-        let input = bu.parseInput(this.flags, words);
-        if (input.undefined) {
-            let emoji = bu.findEmoji(input.undefined[0]);
-            if (emoji.length > 0) {
-                if (emoji[0].startsWith('a:') || emoji[0].startsWith(':')) {
-                    let url = `https://cdn.discordapp.com/emojis/${emoji[0].match(/.*:(\d+)/)[1]}.${emoji[0][0] == 'a' ? 'gif' : 'png'}`;
-                    bu.send(msg, {
-                        embed: {
-                            image: {
-                                url
-                            }
-                        }
-                    });
-                } else {
-                    let codePoint = twemoji.convert.toCodePoint(emoji[0]);
-                    let file = path.join(__dirname, '..', '..', 'node_modules', 'twemoji', '2', 'svg', codePoint + '.svg');
-                    if (fs.existsSync(file)) {
-                        let body = fs.readFileSync(file);
-                        if (input.s) {
-                            bu.send(msg, undefined, {
-                                name: 'emoji.svg',
-                                file: body
-                            });
-                        } else {
-                            let size = 668;
-                            if (input.undefined[1]) {
-                                let tempSize = parseInt(input.undefined[1]);
-                                if (!isNaN(tempSize)) size = tempSize;
-                            }
-                            let buffer = await svg2png(body, {
-                                width: size,
-                                height: size
-                            });
-                            bu.send(msg, '', {
-                                name: 'emoji.png',
-                                file: buffer
-                            });
-                        }
-                    } else {
-                        bu.send(msg, 'Invalid emoji!');
-                    }
-                }
-            } else {
-                bu.send(msg, 'No emoji found!');
-            }
-        } else {
-            bu.send(msg, 'Not enough arguments!');
-        }
-    }
-}
+  
   
   
 client.on("message", async message => {
@@ -373,6 +299,3 @@ client.on("guildCreate", guild => {
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-});
-
-client.login(process.env.BOT_TOKEN);
