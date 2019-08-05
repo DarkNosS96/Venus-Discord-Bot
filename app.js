1d6bc37e2aa09963371b61670d9735da96e4a15e
DarkNosS96 <philipemmil@gmail.com>
2019-08-06 01:09:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const prefix = process.env.prefix || config.prefix;
 const roles = process.env.roles || config.roles;
 const { Client, MessageAttachment } = require('discord.js');
 
-Discord.client.setUsername("Venus");
+client.user.setUsername("Venus");
 
 var updateInterval = (1000 * 60) * 6;
 
