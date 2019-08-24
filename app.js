194a913ed9c403ba83c5fb0ae5f9928e7b6964e2
DarkNosS96 <philipemmil@gmail.com>
2019-08-24 04:27:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -103,12 +103,20 @@ client.on("message", async message => {
 let messageArray = message.content.split(" ");
 let cmd = messageArray[0];
 if(cmd === `!ip`){
-	let small = new Discord.RichEmbed()
+	let embed = new Discord.RichEmbed()
 	.setAuthor("Venus x5 TRIO - CCTV|CARS|iCRAFT", "https://i.imgur.com/rzWmJ6X.png")
 	.setTitle("☆Loot++ ☆Clans ☆AutoDoors ☆BGrade ☆CCTV Cameras ☆Cars ☆InstaCraft ☆QuickSmelt ☆Teleport/Homes")
 	.setColor(0x30bdff)
 	.addField("195.201.80.231:30716", "client.connect 195.201.80.231:30716", false)
-	message.channel.send({small});
+	message.channel.send({embed});
+	
+	
+	let embed = new Discord.RichEmbed()
+	.setAuthor("Venus Battlefield x1000|CCTV|Cars|NO BP/WB", "https://i.imgur.com/rzWmJ6X.png")
+	.setTitle("☆Clans ☆Friends ☆AutoDoors ☆BGrade ☆CCTV Cameras ☆Cars ☆InstaCraft ☆QuickSmelt ☆Teleport/Homes ☆No Blueprints ☆No Workbench")
+	.setColor(0x30bdff)
+	.addField("195.201.80.231:30716", "client.connect 195.201.80.231:30716", false)
+	message.channel.send({embed});
 }
 if(cmd === `!vote`){
 	return message.channel.send("https://venusrust.eu/vote");
